1aff18650fe4e9396f41785c3507a3ea58572a7f
luciid3 <spoonster99@gmail.com>
2016-11-07 15:31:11 +0000

    Ben_15:31

---
+++ Website/javascript.js
@@ -0,0 +1,17 @@
+function replace(){
+	
+	alert("TEST");
+	
+}
+	
+function test(){
+	var a = 5;
+	var b = 6;
+	var c = a+b;
+	return "Alert test";
+}
+
+function changeElement(){
+	document.getElementById("test").innerHTML = "CHANGED!";
+}
+
---
+++ project_petar/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>project24</title>
+  <link rel="stylesheet" href="group.css">
+</head>
+<body>
+  <header>
+    <h1>Project <span id="h1add">24</span></h1>
+    <p id="h1imptxt">This website is part of assignment given by Dr. Shujun Li( 李树钧 )</p>
+    <hr>
+  </header>
+  
+ 
+  <ul class="top">
+    <li><a title="Maddie`s WEB page!" href="#Maddie"><div class="top">AboutMaddie</div></a></li>
+    <li><a title="Ben`s WEB page!" href="#Ben"><div class="top">AboutBen</div></a></li>
+    <li><a title="Harry`s WEB page!" href="#Harry"><div class="top">AboutHarry</div></a></li>
+    <li><a title="Filip`s WEB page!" href="#Filip"><div class="top">AboutFilip</div></a></li>
+    <li><a title="Adrien`s WEB page!" href="#Adrien"><div class="top">AboutAdrien</div></a></li>
+    <li><a title="Petar`s WEB page" href="#Petar"><div class="top">AboutPetar</div></a></li>
+   
+      </ul>
+  <hr>
+  
+  <div class="nav">
+    <h3 id=navh1>Useful links:</h3>
+    <ul>
+      <li><a href="https://www.surrey.ac.uk/"><div class="link">Uni of Surrey</div></a></li>
+      <li><a href="http://www.hooklee.com/"><div class="link">About the lecturer</div></a></li>
+      <li><a href="http://www.w3schools.com/"><div class="link">Free WEB tutoruals</div></a>
+      
+    </ul>
+  </div>
+  <article>
+    <h1>About <span id="ah1">Group 24:</span></h1>
+    <pre>Gropu 24 consists of 6 really devoted students. Harry is the leader of the goup responsible
+for all meetings and </pre>
+  </article>
+  <script src="script-1.js">
+  </script>
+  
+  
+</body>
+</html>
