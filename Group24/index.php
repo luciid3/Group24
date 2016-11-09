f9f19b7318550e749f5ff4a343a37a70aaf05618
luciid3 <spoonster99@gmail.com>
2016-11-09 16:11:22 +0000

    Update index.php

--- Group24/index.php
+++ Group24/index.php
@@ -1,12 +1,15 @@
 <html>
 <head>
+	<meta charset="UTF-8">
+	<title>CW1 Website</title>
+	<link href="stylesheet.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-  <?php
+  	<?php
 	include 'header.php';
-  ?>
-  <?php
+  	?>
+ 	<?php
 	include 'footer.php';
-  ?>
+ 	?>
 </body>
 </html>
