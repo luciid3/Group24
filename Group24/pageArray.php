593936462b0db722e04a14a3f4d3e19b35003dc7
harrySurrey <harryrdm@gmail.com>
2016-11-10 10:16:50 +0000

    Add files via upload

---
+++ Group24/pageArray.php
@@ -0,0 +1,11 @@
+<?php
+	$cookieInfo = array(
+		"home" => "the index page",
+		"harry" => "Harry's individual page",
+		"ben" => "Ben's individual page",
+		"filip" => "Filip's individual page",
+		"petar" => "Petar's individual page",
+		"maddie" => "Maddie's individual page",
+		"adrien" => "Adrien's individual page"
+		);
+?>
